1afa1087b5a95ecff83f3fdefc074bc23876a002
hamdanibp <hamdanibelajarcoding@gmail.com>
2022-06-09 21:36:11 +0700

    sudah kayaknya

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = promiseTheaterIXX()
+    .then(results => results.map(result => result.hasil))
+
+  const vgc = promiseTheaterVGC()
+    .then(results => results.map(result => result.hasil));
+
+  
+  return Promise.all([ixx, vgc])
+    .then(values => {
+      const items = [];
+      const [ixx, vgc] = values;
+      
+      ixx.map(ixxData => items.push(ixxData));
+      vgc.map(vgcData => items.push(vgcData));
+
+      return items.filter(item => item == emosi).length;
+    })
+};
 
 module.exports = {
   promiseOutput,
